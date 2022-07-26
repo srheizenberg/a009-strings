e98d0c7b3b0d76afa5076b400cfc349637afd559
srheizenberg <cleitonfreelance@gmail.com>
2022-07-25 22:37:10 -0300

    exercicio02-feito

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,23 @@
+/*
+# Exercício 2
+
+Observe a string abaixo.
+
+```jsx
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+```
+
+A partir dela, execute os passos abaixo:
+
+a) Remova o excesso de espaços no final da string;
+
+b) exiba, em um console.log() a quantidade de caracteres da string, antes e depois da remoção dos espaços;
+
+c) Substitua os traços `________` por “sticioso”. */
+
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString.trim());
+console.log(minhaString.length);
+
+const resultado = minhaString.replace("________.      ", "sticioso");
+console.log(resultado);
